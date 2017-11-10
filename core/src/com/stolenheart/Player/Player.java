5a438a4079407dd9b8f25220db86ddc15e150d90
Ryan Walton <000128780@student.baisd.net>
2017-11-10 11:03:03 -0500

    added player and other's
    Other enitites and player class

---
+++ core/src/com/stolenheart/Player/Player.java
@@ -0,0 +1,4 @@
+public player(Texture texture){
+
+
+    }
--- core/src/com/stolenheart/entities/adolf.java
+++ core/src/com/stolenheart/entities/adolf.java
@@ -1,15 +1,4 @@
-package com.stolenheart.enemies;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.math.Vector2;
-import com.stolenheart.entities.BaseEnt;
-import com.stolenheart.util.Collision;
-import com.stolenheart.util.Gravity;
-
-public class wat extends BaseEnt {
+public class easyBois extends BaseEnt {
     private TiledMapTileLayer collisionLayer;
     Vector2 vel = new Vector2();
     float oldX, oldY;
@@ -17,7 +6,7 @@ public class wat extends BaseEnt {
     float sideSpeed = -20;
     boolean wasColliding;
 
-    public wat(Sprite sprite, TiledMapTileLayer collisionLayer){
+    public easyBois(Sprite sprite, TiledMapTileLayer collisionLayer){
         super(sprite);
         this.collisionLayer = collisionLayer;
         wasColliding = false;
@@ -79,4 +68,3 @@ public class wat extends BaseEnt {
     public TiledMapTileLayer getCollisionLayer() {
         return collisionLayer;
     }
-}
